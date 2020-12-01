ebaa44535657d6fbb7a1492f0efd194ea9d222a9
iliassalmi <56584061+iliassalmi@users.noreply.github.com>
2020-12-01 14:15:11 +0100

    footer sor modifier profil

--- main/HTML/vue/db-profile-bis.php
+++ main/HTML/vue/db-profile-bis.php
@@ -117,106 +117,8 @@
 
 
     <!--HEADER SECTION-->
-    <section class="wed-hom-footer">
-        <div class="container">
-            <!--<div class="row">
-				<div class="col-md-12">
-				<h4>About Wedding Planner</h4>
-				<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum</p>
-				<p>more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text</p>
-				<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
-				<p></p>
-			  </div>
-			  </div>-->
-            <div class="row wed-foot-link">
-                <div class="col-md-4 foot-tc-mar-t-o">
-                    <h4>Top Courses</h4>
-                    <ul>
-                        <li><a href="course-details.php">Accounting/Finance</a></li>
-                        <li><a href="course-details.php">civil engineering</a></li>
-                        <li><a href="course-details.php">Art/Design</a></li>
-                        <li><a href="course-details.php">Marine Engineering</a></li>
-                        <li><a href="course-details.php">Business Management</a></li>
-                        <li><a href="course-details.php">Journalism/Writing</a></li>
-                        <li><a href="course-details.php">Physical Education</a></li>
-                        <li><a href="course-details.php">Political Science</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <h4>New Courses</h4>
-                    <ul>
-                        <li><a href="course-details.php">Sciences</a></li>
-                        <li><a href="course-details.php">Statistics</a></li>
-                        <li><a href="course-details.php">Web Design/Development</a></li>
-                        <li><a href="course-details.php">SEO</a></li>
-                        <li><a href="course-details.php">Google Business</a></li>
-                        <li><a href="course-details.php">Graphics Design</a></li>
-                        <li><a href="course-details.php">Networking Courses</a></li>
-                        <li><a href="course-details.php">Information technology</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <h4>HELP & SUPPORT</h4>
-                    <ul>
-                        <li><a href="#">24x7 Live help</a>
-                        </li>
-                        <li><a href="#">Contact us</a>
-                        </li>
-                        <li><a href="#">Feedback</a>
-                        </li>
-                        <li><a href="#">FAQs</a>
-                        </li>
-                        <li><a href="#">Safety Tips</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="row wed-foot-link-1">
-                <div class="col-md-4 foot-tc-mar-t-o">
-                    <h4>Get In Touch</h4>
-                    <p>Address: 28800 Orchard Lake Road, Suite 180 Farmington Hills, U.S.A.</p>
-                    <p>Phone: <a href="#!">+101-1231-4321</a></p>
-                    <p>Email: <a href="#!">info@educationmaster.com</a></p>
-                </div>
-                <div class="col-md-4">
-                    <h4>DOWNLOAD OUR FREE MOBILE APPS</h4>
-                    <ul>
-                        <li><a href="#"><span class="sprite sprite-android"></span></a>
-                        </li>
-                        <li><a href="#"><span class="sprite sprite-ios"></span></a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <h4>SOCIAL MEDIA</h4>
-                    <ul>
-                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--END HEADER SECTION-->
+    <?php include('main/HTML/vue/footer.php'); ?>
 
-    <!--HEADER SECTION-->
-    <section class="wed-rights">
-        <div class="container">
-            <div class="row">
-                <div class="copy-right">
-                    <p>Copyrights © 2018 RN53Themes. All rights reserved.</p>
-                </div>
-            </div>
-        </div>
-    </section>
     <!--END HEADER SECTION-->
 
     <!--SECTION LOGIN, REGISTER AND FORGOT PASSWORD-->
@@ -365,7 +267,7 @@
         </div>
     </section>
 
-    
+
 
     <!--Import jQuery before materialize.js-->
     <script src="../js/main.min.js"></script>
